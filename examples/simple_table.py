dc5d553be8dff76239f52de795f37d81eb17e972
Bert Blommers <info@bertblommers.nl>
2019-09-27 08:50:55 +0100

    Add example

--- examples/simple_table.py
+++ examples/simple_table.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
+import boto3
 from migrator.dynamodb_migrator import Migrator
+from time import sleep
 from uuid import uuid4
 
 
+dynamodb = boto3.client('dynamodb')
 table_name = str(uuid4())
-migrator = Migrator()
+migrator = Migrator(identifier='make examples/simple_table.py')
 
 
 @migrator.version(1)
@@ -23,3 +26,24 @@ migrator = Migrator()
 def v1(created_table):
     assert created_table['TableName'] == table_name
     assert created_table['TableStatus'] == 'ACTIVE'
+    print("===================")
+    print("Script has finished")
+    print("We can now use the created table as appropriate")
+    print("As this is only an example, we'll delete the tables again, so that we're not incurring unexpected costs")
+    print("This might take a while...")
+    delete_table(table_name)
+    delete_table('dynamodb_migrator_metadata')
+
+
+def delete_table(name):
+    try:
+        dynamodb.delete_table(TableName=name)
+        while True:
+            dynamodb.describe_table(TableName=name)
+            sleep(1)
+    except dynamodb.exceptions.ResourceNotFoundException:
+        # Table might not exist (anymore)
+        pass
+
+
+migrator.migrate()
