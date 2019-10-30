b68817ac5ec2c313daabd40cc33e8fb001597698
Bert Blommers <info@bertblommers.nl>
2019-10-30 08:27:10 +0000

    Remove duplicate logging

--- src/migrator/dynamodb_migrator.py
+++ src/migrator/dynamodb_migrator.py
@@ -1,21 +1,15 @@
-import logging
 import os
 from migrator.exceptions.MigratorScriptException import MigratorScriptException
 from migrator.steps.BaseStep import BaseStep
 from migrator.steps.CreateTableStep import CreateTableStep
 from migrator.steps.AddIndexStep import AddIndexStep
+from migrator.utilities.Utilities import logger
 
 
 class Migrator():
     _metadata_table_name = 'dynamodb_migrator_metadata'
-    _ch = logging.StreamHandler()
-    _formatter = logging.Formatter('%(asctime)s %(levelname)8s %(name)s | %(message)s')
-    _logger = logging.getLogger('dynamodb_migrator_library')
 
     def __init__(self, identifier = None):
-        self._ch.setFormatter(self._formatter)
-        self._logger.addHandler(self._ch)
-        self._logger.setLevel(logging.DEBUG)
         self._functions = []
         self._steps = []
         self._version = None
@@ -32,9 +26,9 @@ class Migrator():
 
     def create(self, **kwargs):
         if self._table_created:
-            self._logger.error("Unable to execute script")
-            self._logger.error("Ensure that you have only one create-annotation per script")
-            self._logger.error("Each table should have it's own script")
+            logger.error("Unable to execute script")
+            logger.error("Ensure that you have only one create-annotation per script")
+            logger.error("Each table should have it's own script")
             raise MigratorScriptException("Unable to create multiple tables per script")
 
         def inner_function(function):
